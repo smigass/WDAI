f9e6a03c4e35a228ecef5788619d7ac151089759
Szymon Migas <ataszymon@gmail.com>
2024-11-04 19:59:41 +0100

    a

--- mini-project/README.md
+++
@@ -1,2 +0,0 @@
-# mini-project
-npm install
--- mini-project/src/js/main.js
+++
@@ -1,9 +0,0 @@
-// Import our custom CSS
-import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-window.onload = () => {
-    console.log(document.getElementsByClassName("btn-cart"))
-}
-
